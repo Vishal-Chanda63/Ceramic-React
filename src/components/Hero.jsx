eb66e0797b6bd0238d5af19df6f3c32c4f55ad7c
vishal <vishalchanda2018@gmail.com>
2024-07-10 11:31:58 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,92 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-      <>
-          <section>
-              <div className="container">
-                  <div className="row">
-                      <div className="col-6">
-                          <div className='hero_bg'>
-                                <h1>
-                              Transform Your Space with Precision and Quality:
-Get a Free Quote Today!
-                              </h1>
-                              <p>Lörem ipsum koda astrobel: sutaveligen. Rodod bänera viliga. Pregigt primasofi dede facebooka: förutom tivaligt. Fejkade nyheter eurosamma sol: teroktig,</p>
-                              <button>
-                                  Get A Quote
-                              </button>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-      </section>
-      
-      </>
-  )
-}
+    <>
+      <section className="hero_bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-9">
+              <div>
+                <h1 className="white fw_600 font_xxl lh-sm">
+                                  Transform Your Space with Precision and Quality: <span className="d-block ">
+                                      Get a Free
+                  Quote Today!
+                  </span>
+                </h1>
+                <p className="white mb-4">
+                  Lörem ipsum koda astrobel: sutaveligen. Rodod bänera viliga.
+                  Pregigt primasofi dede facebooka: förutom tivaligt. Fejkade
+                  nyheter eurosamma sol: teroktig,
+                </p>
+                              <button className="common_btn font_md fw-bold">Get A Quote</button>
+                              
+                                  <div class="mt-5 pt-0 pt-sm-3 d-flex flex-column flex-md-row">
+                            <div class="d-inline d-md-flex  mb-4">
+                                <div
+                                    class="google_review_box cursur d-inline-block me-sm-4 mb-4 mb-sm-0 review_padding_right">
+                                    <div class="d-flex align-items-center">
+                                        <div class="me-2 pe-1">
+                                          
+                                        </div>
 
-export default Hero
+                                        <div>
+                                            <p class="mb-0 manrope_font font_xsm dark_black text-start">Google Review
+                                            </p>
+
+                                            <div class="d-flex">
+                                                <p class="mb-0 manrope_font font_lg dark_black me-1">4.6</p>
+                                                <div>
+                                                  
+                                                </div>
+                                            </div>
+
+                                            <p class="mb-0 manrope_font font_xsm dark_black">Based on 145 reviews</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="google_review_box cursur d-inline-block me-0 me-sm-4 ">
+                                    <div class="d-flex align-items-center">
+                                        <div class="me-2 pe-1">
+                                           
+
+
+                                        </div>
+
+                                        <div>
+                                            <p class="mb-0 manrope_font font_xsm dark_black text-start">Trust Pilot
+                                                Review
+                                            </p>
+
+                                            <div class="d-flex">
+                                                <p class="mb-0 manrope_font font_lg dark_black me-1">4.6</p>
+                                                <div>
+                                                   
+                                                </div>
+                                            </div>
+
+                                            <p class="mb-0 manrope_font font_xsm dark_black">Based on 145 reviews</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="d-flex justify-content-center justify-content-sm-start">
+                                <div class="me-4 mb-4 mb-sm-0">
+                                
+                                </div>
+                                <div class="me-0 me-sm-4">
+                                  
+                                </div>
+                            </div>
+                        </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
