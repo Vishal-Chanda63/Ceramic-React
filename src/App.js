43bb1a6c6ca3d71ee12681c245db45865fbdcf3b
vishal <vishalchanda2018@gmail.com>
2024-07-11 16:46:07 +0530

    faq

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from "./components/Footer";
 import Whatwedo from "./components/Whatwedo";
 import Ourblog from "./components/Ourblog";
 import Services from "./components/Services";
+import Faq from "./components/Faq";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <Works />
       <Gettouch />
       <Ourblog />
+      <Faq />
       <Services />
       <Footer />
     </>
